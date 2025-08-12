f09962839ca7c1c8bb748080032267f337e2739e
EienMiku <EienMiku@outlook.com>
2025-08-13 02:05:41 +0800

    fix: initialize none_opt with none instead of default constructor
    - Fixes GCC constexpr compilation error where none_opt<T>{} was not considered a constant expression
    - Uses none_t to initialize none_opt<T> instead of default constructor
    - Resolves error: 'opt::option<T>()' is not a constant expression because it refers to an incompletely initialized variable

--- src/include/option.hpp
+++ src/include/option.hpp
@@ -3176,7 +3176,7 @@ namespace opt {
     }
 
     template <typename T>
-    inline constexpr option<T> none_opt{};
+    inline constexpr option<T> none_opt{ none };
 
     template <typename T>
         requires (!detail::specialization_of<std::optional, T>)
